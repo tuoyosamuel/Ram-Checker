8d4a760c8f480c980ececf830d22d130895aac66
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-05-13 18:48:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById("btn");
 btn.addEventListener('click', function Ram(){
     const memory = navigator.deviceMemory;
-    const mem = memory + 1;
+    const mem = memory;
     const Ram = `This Device has a total of ${mem}Gb Ram`;
     alert(Ram);
     
